40a229b5ff28482060f19c2d634f72c2bff926bf
Lekkelmendi7 <lek.kelmendi8@gmail.com>
2024-10-23 15:20:21 +0200

    Initial commit

---
+++ FormulaOne.ChatService/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using FormulaOne.ChatService.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace FormulaOne.ChatService.Hubs
+{
+    public class ChatHub : Hub
+    { 
+        public async Task JoinChat(UserConnection conn)
+        {
+            await Clients.All
+                .SendAsync(method: "RecieveMessage", arg1: "admin", arg2: $"{conn.Username} has joined");
+        }
+
+        public async Task JoinSpecificChatRoom(UserConnection conn)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName: conn.ChatRoom);
+            await Clients.Group(conn.ChatRoom).SendAsync(method: "RecieveMessage", arg1:"admin", arg2:$"{conn.Username} has joined {conn.ChatRoom}");
+        }
+    }
+}
---
+++ FormulaOne.ChatService/Models/UserConnection.cs
@@ -0,0 +1,9 @@
+﻿namespace FormulaOne.ChatService.Models
+{
+    public class UserConnection
+    {
+        public string Username { get; set; }    
+        public string ChatRoom {  get; set; }
+
+    }
+}
